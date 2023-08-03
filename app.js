d826a7cd2f08b3d65575e68967660e84f1af531b
Heber <116190313+hpagon@users.noreply.github.com>
2023-08-02 20:15:24 -0700

    Overhauled playRound function to return number
    This is so we don't have to deal with checking strings in future

--- app.js
+++ app.js
@@ -5,49 +5,51 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
-
 function playRound(playerSelection, computerSelection) {
-  let message;
+//   let message;
   let winner;
   let playerChoice = playerSelection.toLowerCase();
   switch (playerChoice) {
     case "rock":
-      if (computerSelection === "scissors") winner = outcomes[0];
-      else if (computerSelection === "paper") winner = outcomes[1];
-      else if (computerSelection == "rock") {
-        winner = outcomes[2];
-        return winner;
-      }
+      if (computerSelection === "scissors") winner = 0;
+      else if (computerSelection === "paper") winner = 1;
+      else if (computerSelection == "rock") winner = 2;
       break;
     case "paper":
-      if (computerSelection === "rock") winner = outcomes[0];
-      else if (computerSelection === "scissors") winner = outcomes[1];
-      else if (computerSelection == "paper") {
-        winner = outcomes[2];
-        return winner;
-      }
+      if (computerSelection === "rock") winner = 0;
+      else if (computerSelection === "scissors") winner = 1;
+      else if (computerSelection == "paper") winner = 2;
       break;
     case "scissors":
-      if (computerSelection === "rock") winner = outcomes[1];
-      else if (computerSelection === "paper") winner = outcomes[0];
-      else if (computerSelection == "scissors") {
-        winner = outcomes[2];
-        return winner;
-      }
+      if (computerSelection === "rock") winner = 1;
+      else if (computerSelection === "paper") winner = 0;
+      else if (computerSelection == "scissors") winner = 2;
       break;
     default:
-      message = "Unknown input";
+      winner = "-1";
       break;
   }
-  if (winner === outcomes[0]) {
-    message = winner + " " + playerSelection + " beats " + computerSelection;
-  } else if (winner === outcomes[1]) {
-    message = winner + " " + computerSelection + " beats " + playerSelection;
-  }
-  return message;
+  //   if (winner === outcomes[0]) {
+  //     message = winner + " " + playerSelection + " beats " + computerSelection;
+  //   } else if (winner === outcomes[1]) {
+  //     message = winner + " " + computerSelection + " beats " + playerSelection;
+  //   }
+  //   message = winner;
+  //   return message;
+  return winner;
 }
 
-function game() {}
+console.log(playRound(getComputerChoice(), getComputerChoice()))
 
-console.log(playRound(getComputerChoice(), getComputerChoice()));
+function game() {
+  let userInput;
+  let compInput;
+  let playerScore = 0;
+  let computerScore = 0;
+
+  console.log("Let's play Rock Paper Scissors");
+  for (let i = 0; i < 5; i++) {
+    userInput = prompt("Your turn, you choose...", getComputerChoice());
+    compInput = getComputerChoice();
+  }
+}
