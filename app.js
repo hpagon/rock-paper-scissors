a6ae41c08de6997e39a931c43e37119084237d7c
Heber <116190313+hpagon@users.noreply.github.com>
2023-08-03 13:52:35 -0700

    Created CSS file and created HTML layout
    The CSS file and HTML is for later styling/ui

--- app.js
+++ app.js
@@ -31,55 +31,55 @@ function playRound(playerSelection, computerSelection) {
   return winner;
 }
 
-console.log(playRound(getComputerChoice(), getComputerChoice()));
+// console.log(playRound(getComputerChoice(), getComputerChoice()));
 
-function game() {
-  let userInput;
-  let compInput;
-  let result;
-  let resultMessage;
-  let playerScore = 0;
-  let computerScore = 0;
+// function game() {
+//   let userInput;
+//   let compInput;
+//   let result;
+//   let resultMessage;
+//   let playerScore = 0;
+//   let computerScore = 0;
 
-  console.log("Let's play Rock Paper Scissors");
-  for (let i = 0; i < 5; i++) {
-    userInput = prompt("Your turn, you choose...", getComputerChoice());
-    compInput = getComputerChoice();
-    result = playRound(userInput, compInput);
-    if (result === 0) {
-      playerScore++;
-      console.log(
-        `${outcomes[0]} ${userInput.toLowerCase()} beats ${compInput}`
-      );
-      console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-      if (playerScore === 3) {
-        console.log(`Player wins the match - ${playerScore}:${computerScore}`);
-        return;
-      }
-    } else if (result === 1) {
-      computerScore++;
-      console.log(
-        `${outcomes[1]} ${compInput} beats ${userInput.toLowerCase()}`
-      );
-      console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-      if (computerScore === 3) {
-        console.log(`Player lost the match - ${playerScore}:${computerScore}`);
-        return;
-      }
-    } else if (result === 2) {
-      console.log(
-        `${outcomes[2]} ${userInput.toLowerCase()} ties with ${compInput}`
-      );
-      console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-    }
-  }
-  if (playerScore > computerScore) {
-    console.log(`Player wins the match - ${playerScore}:${computerScore}`);
-  } else if (playerScore < computerScore) {
-    console.log(`Player lost the match - ${playerScore}:${computerScore}`);
-  } else {
-    console.log(`Tie. No one wins - ${playerScore}:${computerScore}`);
-  }
-}
+//   console.log("Let's play Rock Paper Scissors");
+//   for (let i = 0; i < 5; i++) {
+//     userInput = prompt("Your turn, you choose...", getComputerChoice());
+//     compInput = getComputerChoice();
+//     result = playRound(userInput, compInput);
+//     if (result === 0) {
+//       playerScore++;
+//       console.log(
+//         `${outcomes[0]} ${userInput.toLowerCase()} beats ${compInput}`
+//       );
+//       console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+//       if (playerScore === 3) {
+//         console.log(`Player wins the match - ${playerScore}:${computerScore}`);
+//         return;
+//       }
+//     } else if (result === 1) {
+//       computerScore++;
+//       console.log(
+//         `${outcomes[1]} ${compInput} beats ${userInput.toLowerCase()}`
+//       );
+//       console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+//       if (computerScore === 3) {
+//         console.log(`Player lost the match - ${playerScore}:${computerScore}`);
+//         return;
+//       }
+//     } else if (result === 2) {
+//       console.log(
+//         `${outcomes[2]} ${userInput.toLowerCase()} ties with ${compInput}`
+//       );
+//       console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+//     }
+//   }
+//   if (playerScore > computerScore) {
+//     console.log(`Player wins the match - ${playerScore}:${computerScore}`);
+//   } else if (playerScore < computerScore) {
+//     console.log(`Player lost the match - ${playerScore}:${computerScore}`);
+//   } else {
+//     console.log(`Tie. No one wins - ${playerScore}:${computerScore}`);
+//   }
+// }
 
-game();
+// game();
