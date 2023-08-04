171eecbbf639a5d739361930a17781839ae74f70
Heber <116190313+hpagon@users.noreply.github.com>
2023-08-04 08:29:34 -0700

    Styled modal and changed font
    Font was changed from default and modal was styled to match page

--- app.js
+++ app.js
@@ -65,6 +65,16 @@ function clicked(e) {
       computerSign.textContent = computerScore;
       break;
   }
+  if (playerScore === 5) {
+    modalText.textContent = `Match Won ${playerScore}:${computerScore}`;
+    modal.showModal();
+    return;
+  }
+  if (computerScore === 5) {
+    modalText.textContent = `Match Lost ${playerScore}:${computerScore}`;
+    modal.showModal();
+    return;
+  }
 }
 
 function playAgain() {
@@ -72,6 +82,7 @@ function playAgain() {
   computerScore = 0;
   playerSign.textContent = playerScore;
   computerSign.textContent = computerScore;
+  message.textContent = "You choose...";
   giveEvents();
 }
 
@@ -97,56 +108,3 @@ modalClose.addEventListener("click", () => {
 });
 
 giveEvents();
-
-// console.log(playRound(getComputerChoice(), getComputerChoice()));
-
-// function game() {
-//   let userInput;
-//   let compInput;
-//   let result;
-//   let resultMessage;
-//   let playerScore = 0;
-//   let computerScore = 0;
-
-//   console.log("Let's play Rock Paper Scissors");
-//   for (let i = 0; i < 5; i++) {
-//     userInput = prompt("Your turn, you choose...", getComputerChoice());
-//     compInput = getComputerChoice();
-//     result = playRound(userInput, compInput);
-//     if (result === 0) {
-//       playerScore++;
-//       console.log(
-//         `${outcomes[0]} ${userInput.toLowerCase()} beats ${compInput}`
-//       );
-//       console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-//       if (playerScore === 3) {
-//         console.log(`Player wins the match - ${playerScore}:${computerScore}`);
-//         return;
-//       }
-//     } else if (result === 1) {
-//       computerScore++;
-//       console.log(
-//         `${outcomes[1]} ${compInput} beats ${userInput.toLowerCase()}`
-//       );
-//       console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-//       if (computerScore === 3) {
-//         console.log(`Player lost the match - ${playerScore}:${computerScore}`);
-//         return;
-//       }
-//     } else if (result === 2) {
-//       console.log(
-//         `${outcomes[2]} ${userInput.toLowerCase()} ties with ${compInput}`
-//       );
-//       console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-//     }
-//   }
-//   if (playerScore > computerScore) {
-//     console.log(`Player wins the match - ${playerScore}:${computerScore}`);
-//   } else if (playerScore < computerScore) {
-//     console.log(`Player lost the match - ${playerScore}:${computerScore}`);
-//   } else {
-//     console.log(`Tie. No one wins - ${playerScore}:${computerScore}`);
-//   }
-// }
-
-// game();
